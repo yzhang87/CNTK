0e17ca8b77b9c6b78ac2c9e755b32b1c2d186569
Eldar Akchurin <eldak@microsoft.com>
2015-11-11 09:09:49 +0100

    Removing commented code

--- Math/Math/GPUSparseMatrix.cu
+++ Math/Math/GPUSparseMatrix.cu
@@ -14,7 +14,6 @@
 #include <cuda_runtime.h>
 #include <cusparse_v2.h>
 #include "cublas_v2.h"
-//#include "GPUMatrixCUDAKernels.cu"
 #include "GPUMatrixCUDAKernels.cuh"
 #include <functional>
 #include "CommonMatrix.h"
