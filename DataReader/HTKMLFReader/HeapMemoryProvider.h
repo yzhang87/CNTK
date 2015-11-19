e8ed0124b99615ba79785694adcebbc5544e3fab
Eldar Akchurin <eldak@microsoft.com>
2015-11-19 15:02:21 +0100

    Fixing memory provider

--- DataReader/HTKMLFReader/HeapMemoryProvider.h
+++ DataReader/HTKMLFReader/HeapMemoryProvider.h
@@ -13,13 +13,13 @@ namespace Microsoft { namespace MSR { namespace CNTK {
     public:
         virtual void* allocate(size_t elmentSize, size_t numberOfElements) override
         {
-            size_t allignment = std::max(elmentSize, size_of_first_pointer);
+            size_t allignment = max(elmentSize, size_of_first_pointer);
             size_t request_size = elmentSize * numberOfElements + allignment;
             size_t needed = size_of_first_pointer + request_size;
 
             void* allocted = ::operator new(needed);
             void* allowed_space = reinterpret_cast<char*>(allocted) + size_of_first_pointer;
-            void* p = std::align(elmentSize, elmentSize, allowed_space, request_size);
+            void* p = std::align(allignment, elmentSize, allowed_space, request_size);
 
             // save for delete calls to use
             (reinterpret_cast<void**>(p))[-1] = allocted;
