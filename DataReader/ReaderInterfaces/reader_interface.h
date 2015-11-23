877264975cb349afe1bfeff0c22e1b76fa85fd77
Eldar Akchurin <eldak@microsoft.com>
2015-11-23 17:13:22 +0100

    More comments

--- DataReader/ReaderInterfaces/reader_interface.h
+++ DataReader/ReaderInterfaces/reader_interface.h
@@ -8,6 +8,7 @@ class config_parameters
 {
 };
 
+// Epoch configuration.
 struct epoch_configuration
 {
     size_t worker_rank;
@@ -19,6 +20,7 @@ struct epoch_configuration
     size_t number_of_sequences;
 };
 
+// Input description.
 struct input_description
 {
     std::string name;
@@ -44,6 +46,7 @@ class layout
 
 typedef std::shared_ptr<layout> layout_ptr;
 
+// Input data.
 class input
 {
     const char* get_data() const;
@@ -52,6 +55,7 @@ class input
 };
 typedef std::shared_ptr<input> input_ptr;
 
+// Memory provider. Should be used for allocating storage according to the layout.
 class memory_provider
 {
 public:
@@ -60,6 +64,7 @@ public:
 };
 typedef std::shared_ptr<memory_provider> memory_provider_ptr;
 
+// Represents a single epoch.
 class epoch
 {
 public:
@@ -68,7 +73,7 @@ public:
 };
 typedef std::unique_ptr<epoch> epoch_ptr;
 
-
+// Main reader interface. The border interface between the CNTK and reader.
 class reader
 {
 public:
@@ -78,4 +83,6 @@ public:
 };
 typedef std::unique_ptr<reader> reader_ptr;
 
+
+// Factory function for creating a reader.
 reader_ptr create_reader(const config_parameters& parameters, memory_provider_ptr memory_provider);
