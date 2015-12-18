c48b750a9d7c0859fd08201db93578e494becb82
Mark Hillebrand <Mark.Hillebrand@microsoft.com>
2015-12-18 14:28:07 +0100

    ImageDeserializer: add stubs

--- DataReader/NewImageReader/ImageDeserializer.h
+++ DataReader/NewImageReader/ImageDeserializer.h
@@ -1,4 +1,21 @@
 #pragma once
 
+#include "InnerInterfaces.h"
+#include "commandArgUtil.h"
+
 namespace Microsoft { namespace MSR { namespace CNTK {
+
+    class ImageDataDeserializer : public DataDeserializer
+    {
+        TimelineP m_sequences;
+    public:
+        ImageDataDeserializer(const ConfigParameters& config); // TODO more
+
+        std::vector<InputDescriptionPtr> GetInputs() const override;
+        void SetEpochConfiguration(const EpochConfiguration& config) override;
+        const TimelineP& GetSequenceDescriptions() const override;
+        std::vector<Sequence> GetSequenceById(size_t id) override;
+        bool RequireChunk(size_t chunkIndex) override;
+        void ReleaseChunk(size_t chunkIndex) override;
+    };
 }}}
