8572b3720215863226116ba7e192a1ebe37b1c8b
Mark Hillebrand <Mark.Hillebrand@microsoft.com>
2015-12-18 15:50:27 +0100

    Start adding init code to ImageDataDeserializer

--- DataReader/NewImageReader/ImageDeserializer.cpp
+++ DataReader/NewImageReader/ImageDeserializer.cpp
@@ -3,8 +3,63 @@
 
 namespace Microsoft { namespace MSR { namespace CNTK {
 
-    ImageDataDeserializer::ImageDataDeserializer(const ConfigParameters& /* config */)
+    ImageDataDeserializer::ImageDataDeserializer(const ConfigParameters& config)
     {
+        using SectionT = std::pair<std::string, ConfigParameters>;      // TODO: does not work for BrainScript, since configs cannot be copied
+        auto getter = [&](const std::string& paramName) -> SectionT
+        {
+            auto sect = std::find_if(config.begin(), config.end(),
+                [&](const std::pair<std::string, ConfigValue>& p)
+                {
+                    return ConfigParameters(p.second).ExistsCurrent(paramName);
+                });
+            if (sect == config.end())
+                RuntimeError("ImageReader requires %s parameter.", paramName.c_str());
+            return{ (*sect).first, ConfigParameters((*sect).second) };
+        };
+
+        // REVIEW alexeyk: currently support only one feature and label section.
+        SectionT featSect{ getter("width") };
+        m_featName = msra::strfun::utf16(featSect.first);
+        // REVIEW alexeyk: w, h and c will be read again in ScaleTransform.
+        size_t w = featSect.second("width");
+        size_t h = featSect.second("height");
+        size_t c = featSect.second("channels");
+        m_featDim = w * h * c;
+        // TODO we should not need this?
+
+        SectionT labSect{ getter("labelDim") };
+        m_labName = msra::strfun::utf16(labSect.first);
+        m_labDim = labSect.second("labelDim");
+
+        std::string mapPath = config(L"file");
+        std::ifstream mapFile(mapPath);
+        if (!mapFile)
+            RuntimeError("Could not open %s for reading.", mapPath.c_str());
+
+        std::string line{ "" };
+
+        ImageSequenceDescription description;
+
+        description.numberOfSamples = 1;
+        description.isValid = true;
+
+        for (size_t cline = 0; std::getline(mapFile, line); cline++)
+        {
+            std::stringstream ss{ line };
+            std::string imgPath;
+            std::string clsId;
+            if (!std::getline(ss, imgPath, '\t') || !std::getline(ss, clsId, '\t'))
+                RuntimeError("Invalid map file format, must contain 2 tab-delimited columns: %s, line: %d.", mapPath.c_str(), cline);
+            description.id = cline;
+            // Put each image in its own chunk
+            description.chunkId = cline;
+            description.path = imgPath;
+            description.classId = std::stoi(clsId);
+            assert(description.classId < m_labDim);
+            m_imageSequences.push_back(description);
+            m_sequences.push_back(&m_imageSequences[cline]);
+        }
     }
 
     std::vector<InputDescriptionPtr> ImageDataDeserializer::GetInputs() const
@@ -15,6 +70,7 @@ namespace Microsoft { namespace MSR { namespace CNTK {
 
     void ImageDataDeserializer::SetEpochConfiguration(const EpochConfiguration& /* config */)
     {
+        throw std::logic_error("The method or operation is not implemented.");
     }
 
     const TimelineP& ImageDataDeserializer::GetSequenceDescriptions() const
--- DataReader/NewImageReader/ImageDeserializer.h
+++ DataReader/NewImageReader/ImageDeserializer.h
@@ -7,7 +7,37 @@ namespace Microsoft { namespace MSR { namespace CNTK {
 
     class ImageDataDeserializer : public DataDeserializer
     {
+        struct ImageSequenceDescription : public SequenceDescription
+        {
+            std::string path;
+            size_t classId;
+        };
+        std::vector<ImageSequenceDescription> m_imageSequences;
+
         TimelineP m_sequences;
+
+        std::wstring m_featName;
+        std::wstring m_labName;
+
+        size_t m_featDim;
+        size_t m_labDim;
+
+        //using StrIntPairT = std::pair<std::string, int>;
+        //std::vector<StrIntPairT> files;
+
+        size_t m_epochSize;
+        size_t m_mbSize;
+        size_t m_epoch;
+
+        size_t m_epochStart;
+        size_t m_mbStart;
+        //std::vector<ElemType> m_featBuf;
+        //std::vector<ElemType> m_labBuf;
+
+        bool m_imgListRand;
+
+        //MBLayoutPtr m_pMBLayout;
+
     public:
         ImageDataDeserializer(const ConfigParameters& config); // TODO more
 
