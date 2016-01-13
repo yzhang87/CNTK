a1c54934110d589f9791023ba5f8d7d40b828aff
Gaizka Navarro <gaizna@microsoft.com>
2016-01-13 17:24:58 +0100

    Added CSharp shim to access CNTK Eval dll

---
+++ Source/CSEvalClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.MSR.CNTK;
+
+namespace CSEvalClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string config = GetConfig();
+            IEvaluateModelManaged model = new IEvaluateModelManaged(config);
+            model.Destroy();
+        }
+
+        static string GetConfig()
+        {
+            var lines = System.IO.File.ReadAllLines(@"E:\VSO\Source\Repos\CNTK_CUDA70\Examples\Other\Simple2d\Config\Simple.config");
+            return string.Join("", lines);
+        }
+    }
+}
